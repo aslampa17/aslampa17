b90226e2bdd8ff949eafd9643b3cb790b61966e2
Aslam <93756533+LW1117@users.noreply.github.com>
2025-05-18 10:33:41 +0530

    Add logo to navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom"
+import profilePic from "./assets/propic.jpg"
 
 function Navbar(){
 
     return(
         <div className="navbar">
+            <div className="logo">
+                <img src={profilePic} alt="" />
+                P A Aslam Mohammed
+            </div>
             <ul className="nav-links">
                 <li><Link to="/aslampa17">Home</Link></li>
                 <li><Link to="/projects">Projects</Link></li>
